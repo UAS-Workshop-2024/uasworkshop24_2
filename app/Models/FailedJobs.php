7f3b1b9dba6cc87d6eb5e72b2852275a70094660
aishalaily <149878162+aishalaily@users.noreply.github.com>
2024-11-21 23:22:22 +0700

    Membuat models

---
+++ app/Models/FailedJobs.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FailedJobs extends Model
+{
+    use HasFactory;
+    protected $table = 'failed_jobs';
+    protected $primaryKey = 'id';
+    protected $guarded = [
+
+    ];
+}
